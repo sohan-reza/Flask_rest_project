3bd701611869863e5632b5cc47a33120689170e4
Sohan Reza <sohanreja22@gmail.com>
2022-09-08 19:58:31 +0600

    store data in a python list insted of database and write the first route

--- code/app.py
+++ code/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+##App Model
+
+tasks= [
+    {
+        'id':1,
+        'title': 'Write the code',
+        'description': 'Code the todo app and push this to github',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Learn about REST',
+        'description': 'Read all the bookmarked REST tutorial',
+        'done': False
+    }
+]
+
+
+
+##App Controller
+@app.route('/todo/api/v1.0/tasks/', methods=['GET'])
 def index():
-    return 'Hello World!'
+    return jsonify({'tasks': tasks})
 
 if __name__=='__main__':
     app.run(debug=True)
