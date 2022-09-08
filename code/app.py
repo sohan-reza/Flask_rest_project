7f839aa31ad027adf64cfad781abadcf68fc26bb
Sohan Reza <sohanreja22@gmail.com>
2022-09-08 20:24:01 +0600

    Create another route for fetch/get one data by id and customize the 404 error response

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -22,9 +22,25 @@ tasks= [
 
 
 ##App Controller
-@app.route('/todo/api/v1.0/tasks/', methods=['GET'])
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def index():
     return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id']==task_id]
+    if len(task)==0:
+        abort(404)
+    return jsonify({'task':task[0]})
+
+## Error Handler
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error':'Not Found'}), 404)
+
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
